892114a465d3c928b73649462edb14a32594861d
Paritosh Batish <batishparitosh2@gmail.com>
2020-03-23 20:39:23 +0530

    auto

--- index.js
+++ index.js
@@ -22,12 +22,7 @@ app.post("/api/users/register", async (req, res) => {
   console.log(req.body);
   con.query(
     "INSERT INTO users(phone, name, password, email) values (?, ?, ?, ?)",
-    [
-      req.body.phone,
-      req.body.name,
-      req.body.password,
-      req.body.email
-    ],
+    [req.body.phone, req.body.name, req.body.password, req.body.email],
     (err, result) => {
       if (err) {
         console.log("Error:", err);
@@ -49,12 +44,64 @@ app.post("/api/users/login", async (req, res) => {
       if (err) {
         console.log("Error:", err);
         return res.status(400).send(false);
+      } else {
+        console.log("Result", result);
+        if (result.length === 0) return res.status(400).send(false);
+        else return res.status(200).send(true);
+      }
+    }
+  );
+});
+
+app.post("/api/users/guest", async (req, res) => {
+  console.log(req.body);
+  con.query(
+    "INSERT INTO guests(phone, name, email) values (?, ?, ?)",
+    [req.body.phone, req.body.name, req.body.email],
+    (err, result) => {
+      if (err) {
+        console.log("Error:", err);
+        if (err.code === "ER_DUP_ENTRY") return res.status(200).send(true);
+        return res.status(400).send(false);
+      }
+      console.log("Result", result);
+      return res.status(200).send(true);
+    }
+  );
+});
+
+app.post("/api/managers/register", async (req, res) => {
+  console.log(req.body);
+  con.query(
+    "INSERT INTO managers(phone, name, password, email) values (?, ?, ?, ?)",
+    [req.body.phone, req.body.name, req.body.password, req.body.email],
+    (err, result) => {
+      if (err) {
+        console.log("Error:", err);
+        return res.status(400).send(false);
+      }
+      console.log("Result", result);
+      return res.status(200).send(true);
+    }
+  );
+  res.send("You request is recieved");
+});
+
+app.post("/api/managers/login", async (req, res) => {
+  console.log(req.body);
+  con.query(
+    "SELECT * FROM managers WHERE email=? AND password=?",
+    [req.body.email, req.body.password],
+    (err, result) => {
+      if (err) {
+        console.log("Error:", err);
+        return res.status(400).send(false);
       }
       console.log("Result", result);
       if (result.length === 0) return res.status(400).send(false);
       else return res.status(200).send(true);
     }
-  )
+  );
 });
 
 const port = process.env.PORT || 3000;
